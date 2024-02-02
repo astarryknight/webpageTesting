7181675e88c6ac44828a8c2899a1944127546329
Starry Knight <whiteh4tter@gmail.com>
2024-02-01 23:03:29 -0500

    finish framework (css and js) for dark/light mode toggle button

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+//dark/light mode toggle
+tg = 0;
+
+document.getElementById("thmToggle").addEventListener("click", function (e) {
+    const root = document.querySelector(':root');
+    if (tg % 2 == 0) {
+        root.setAttribute('color-scheme', "dark");
+    }
+    else {
+        root.setAttribute('color-scheme', 'light');
+    }
+    tg++;
+});
+
+
+//nav bar navigation handling
 let nav = document.getElementsByClassName("navEl")
 
 for (el in nav) {
@@ -6,7 +22,8 @@ for (el in nav) {
             return false;
         }
         else {
-            
+            //todo: use e to find which element was clicked, maybe just have spans over all three and set it to have a color
+            //maybe have a variable that stores the postion of the webpage for further use
         }
     });
 }
