ed6ebaf549bbaa4a39dccfeb9ffb3459b9eab8aa
Starry Knight <82652543+astarryknight@users.noreply.github.com>
2024-02-02 16:57:10 +0000

    implement simple navbar navigation and dynamic background image

--- main.js
+++ main.js
@@ -1,31 +1,61 @@
-//dark/light mode toggle
-tg = 0;
+tg = 0; //dark/light mode toggle ctr, %2 to switch between states
+rotInc=90;//background rotation increment (degrees)
 
+//dark/light mode toggle implementation
 document.getElementById("thmToggle").addEventListener("click", function (e) {
-    const root = document.querySelector(':root');
-    if (tg % 2 == 0) {
-        root.setAttribute('color-scheme', "dark");
-    }
-    else {
-        root.setAttribute('color-scheme', 'light');
-    }
-    tg++;
+	const root = document.querySelector(':root');
+	if (tg % 2 == 0) {
+		root.setAttribute('color-scheme', "dark");
+	}
+	else {
+		root.setAttribute('color-scheme', 'light');
+	}
+	tg++;
 });
 
 
 //nav bar navigation handling
-let nav = document.getElementsByClassName("navEl")
+navPos=0; //0 indexed var for page position
 
-for (el in nav) {
-    el.addEventListener("click", function (e) {
-        if ("select" in el.classList) {
-            return false;
-        }
-        else {
-            //todo: use e to find which element was clicked, maybe just have spans over all three and set it to have a color
-            //maybe have a variable that stores the postion of the webpage for further use
-        }
-    });
+function getNavElement(navPos){
+	if(navPos==0){
+		return "nav1";
+	} else if(navPos==1){
+		return "nav2";
+	} else if(navPos==2){
+		return "nav3";
+	} else{
+		return null; //error
+	}
 }
 
+nav = document.getElementsByClassName("nav");
+document.getElementById(getNavElement(navPos)).parentElement.style.background = "#2013BB";
+document.getElementById("bg").setAttribute("transform", "rotate(10)");
 
+for (i=0;i<nav.length;i++) {
+	el=nav[i];
+	if(el.nodeName=="DIV"){
+		el.addEventListener("click", function (e) {
+			document.getElementById(getNavElement(navPos)).classList.remove("select");
+			document.getElementById(getNavElement(navPos)).classList.add("noSelect");
+			document.getElementById(getNavElement(navPos)).parentElement.style.background = "transparent";
+			console.log(e.target.id);
+			if(e.target.id=="nav1"){
+				navPos=0;
+				document.getElementById("bg").style.setProperty('--svg-rot-deg',0+"deg");
+			} else if (e.target.id=="nav2"){
+				navPos=1;
+				document.getElementById("bg").style.setProperty('--svg-rot-deg',rotInc+"deg");
+			} else if (e.target.id=="nav3"){
+				navPos=2;
+				document.getElementById("bg").style.setProperty('--svg-rot-deg',(2*rotInc)+"deg");
+			} else {
+				return null; //error
+			}
+			document.getElementById(getNavElement(navPos)).classList.add("select");
+			document.getElementById(getNavElement(navPos)).classList.remove("noSelect");
+			document.getElementById(getNavElement(navPos)).parentElement.style.background = "#2013BB";
+		});
+	}	
+}
