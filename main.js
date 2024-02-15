badcba8b5ec65761741bd9bf88925439afa1aabc
Starry Knight <82652543+astarryknight@users.noreply.github.com>
2024-02-15 16:43:51 +0000

    begin to fix text color and arrow handling bugs

--- main.js
+++ main.js
@@ -9,11 +9,14 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 //dark/light mode toggle implementation
 document.getElementById("thmToggle").addEventListener("click", function (e) {
     const root = document.querySelector(':root');
+    var b = document.getElementsByClassName("blueBox");
     if (tg % 2 == 0) {
         root.setAttribute('color-scheme', "dark");
+        for(i=0;i<b.length;i++) { b.color="white" } //selector box text color handling
     }
     else {
         root.setAttribute('color-scheme', 'light');
+        for(i=0;i<b.length;i++) { b.color="black" }
     }
     tg++;
 });
@@ -45,7 +48,7 @@ for (i = 0; i < nav.length; i++) {
             document.getElementById(getNavElement(navPos)).classList.remove("select");
             document.getElementById(getNavElement(navPos)).classList.add("noSelect");
             document.getElementById(getNavElement(navPos)).parentElement.style.background = "transparent";
-            console.log(e.target.id);
+            if(tg%2==0) {document.getElementById(getNavElement(navPos)).parentElement.style.color = "black";}
             if (e.target.id == "nav1") {
                 navPos = 0;
                 updateMain();
@@ -63,14 +66,14 @@ for (i = 0; i < nav.length; i++) {
 }
 
 //arrow navigation handling
-document.getElementById("rArrow").addEventListener("click", function (e) {
+document.getElementById("right").addEventListener("click", function (e) {
     document.getElementById(getNavElement(navPos)).classList.remove("select");
     document.getElementById(getNavElement(navPos)).classList.add("noSelect");
     document.getElementById(getNavElement(navPos)).parentElement.style.background = "transparent";
     navPos++;
     updateMain();
 });
-document.getElementById("lArrow").addEventListener("click", function (e) {
+document.getElementById("left").addEventListener("click", function (e) {
     document.getElementById(getNavElement(navPos)).classList.remove("select");
     document.getElementById(getNavElement(navPos)).classList.add("noSelect");
     document.getElementById(getNavElement(navPos)).parentElement.style.background = "transparent";
@@ -78,25 +81,49 @@ document.getElementById("lArrow").addEventListener("click", function (e) {
     updateMain();
 });
 
+function hasClass(element, className){
+    for(i=0;i<element.classList.length;i++) { 
+        if(element.classList[i]==className){
+            return true;
+        }
+    }
+    return false;
+}
+
 //position handling (todo - make sure button can't be clicked even when it's hidden using class "noClick")
 function updateMain() {
+	left = document.getElementById("lArrow");
+	right = document.getElementById("rArrow");
     if (navPos == 0) {
         document.getElementById("bg").style.setProperty('--svg-rot-deg', 0 + "deg");
-        document.getElementById("lArrow").style.opacity = 0;
-        document.getElementById("rArrow").style.opacity = 1;
+        addClass(right, left);
+        left.style.opacity = 0;
+        right.style.opacity = 1;
     } else if (navPos == 1) {
         document.getElementById("bg").style.setProperty('--svg-rot-deg', rotInc + "deg");
-        document.getElementById("lArrow").style.opacity = 1;
-        document.getElementById("rArrow").style.opacity = 1;
+        addClass(right, left);
+        left.style.opacity = 1;
+        right.style.opacity = 1;
     } else if (navPos == 2) {
         document.getElementById("bg").style.setProperty('--svg-rot-deg', (2 * rotInc) + "deg");
-        document.getElementById("lArrow").style.opacity = 1;
-        document.getElementById("rArrow").style.opacity = 0;
+        addClass(right, left);
+        left.style.opacity = 1;
+        right.style.opacity = 0;
     }
     moveNew();
     document.getElementById(getNavElement(navPos)).classList.add("select");
     document.getElementById(getNavElement(navPos)).classList.remove("noSelect");
     document.getElementById(getNavElement(navPos)).parentElement.style.background = "#2013BB";
+    document.getElementById(getNavElement(navPos)).parentElement.style.color = "white";
+}
+
+function addClass(right, left){
+    if(hasClass(left, "noClick")){
+        left.classList.add("noClick");
+    }
+    if(hasClass(right, "noClick")){
+        right.classList.remove("noClick");
+    }
 }
 
 //temp func new
@@ -116,6 +143,8 @@ function moveNew() {
 
 
 //todo - separate by files for ease of access and readability
+
+console.log(hasClass(document.getElementById("rArrow"), "arrow"));
 
 //update page on-load
 updateMain();
